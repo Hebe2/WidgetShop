f1b7bc490beffb5ca51c3bd433f672bd9c89a8f7
Mark George <mark.george@otago.ac.nz>
2026-07-28 13:19:46 +1200

    Updates for 2026: * Update libraries * Migrate to Jetty 12 since Jetty 10 doesn't work on recent JDKs * Refactor build.gradle for config caching

--- src/main/java/JettyServer.java
+++ src/main/java/JettyServer.java
@@ -1,15 +1,9 @@
-import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.plus.webapp.EnvConfiguration;
-import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import java.nio.file.Path;
+import org.eclipse.jetty.ee8.webapp.Configuration;
+import org.eclipse.jetty.ee8.webapp.Configurations;
+import org.eclipse.jetty.ee8.webapp.WebAppContext;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.FragmentConfiguration;
-import org.eclipse.jetty.webapp.MetaInfConfiguration;
-import org.eclipse.jetty.webapp.WebAppConfiguration;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebInfConfiguration;
-import org.eclipse.jetty.webapp.WebXmlConfiguration;
 
 public class JettyServer {
 
@@ -26,20 +20,9 @@ public class JettyServer {
 
 		WebAppContext context = new WebAppContext();
 
-		context.setConfigurations(
-			 new Configuration[]{
-				 new AnnotationConfiguration(),
-				 new WebAppConfiguration(),
-				 new WebXmlConfiguration(),
-				 new WebInfConfiguration(),
-				 new PlusConfiguration(),
-				 new MetaInfConfiguration(),
-				 new FragmentConfiguration(),
-				 new EnvConfiguration()
-			 }
-		);
-
-		context.setResourceBase("build/deploy");
+                context.setConfigurations(Configurations.getServerDefault(server).toArray(new Configuration[0]));
+                
+                context.setBaseResource(context.getResourceFactory().newResource(Path.of("build/deploy")));
 		context.setContextPath(CONTEXT_PATH);
 		context.setParentLoaderPriority(true);
 		server.addConnector(connector);
@@ -47,7 +30,7 @@ public class JettyServer {
 
 		server.start();
 
-		System.out.println("\nMilestone 1 server ready on:\n" + server.getURI());
+		System.out.println("\nStudents System server ready on:\n\n" + server.getURI());
 
 		server.join();
 	}
